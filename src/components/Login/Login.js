1bdf26069f36e7ae39d5959e58f6987002f302f8
Reid McCulloch <rmcc0724@gmail.com>
2021-06-29 23:30:45 -0700

    Use effect Summary

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,6 +12,15 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false)
 
   useEffect(() => {
+    console.log('Effect Running');
+
+    return () => {
+      console.log('Effect CleanUp')
+    }
+  }, []);
+
+
+  useEffect(() => {
     const identifier = setTimeout(() => {
       console.log("Checkig for validity.");
       setFormIsValid(
